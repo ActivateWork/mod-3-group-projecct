78c462f64aaa8fe713ef2e2328d63751fa06a297
FarhadIbrahimov <farhadibrahimov@gmail.com>
2023-08-25 11:10:06 -0600

    added comments

--- client/src/users/Login.jsx
+++ client/src/users/Login.jsx
@@ -3,50 +3,62 @@ import axios from "axios";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+// Initial form state with empty values for username, password, and email
+
 let emptyForm = {
   username: "",
   password: "",
   email: "",
 };
 
+// Login component definition
 function Login({ setUser }) {
   const navigate = useNavigate();
 
+  // State to hold the form data, initialized with emptyForm
   let [form, setForm] = useState(emptyForm);
 
+  // Function to handle input changes and update the form state
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
 
+  // Function to handle form submission
   const handleSubmit = async (e) => {
-    e.preventDefault();
+    e.preventDefault(); // Prevents the default form submission behavior
 
     try {
+      // Send a POST request to the server to authenticate user
       const authResponse = await axios.post("/auth/login", form);
       const token = authResponse.data.token;
 
       if (!token) {
-        setForm(emptyForm);
+        setForm(emptyForm); // Reset the form if authentication fails
         return;
       }
 
+      // If authentication is successful, store the token in local storage
       localStorage.setItem("token", token);
 
+      // Send a GET request to fetch user data using the stored token
       const userResponse = await axios.get("/api/users", {
         headers: {
           Authorization: `Bearer ${localStorage.getItem("token")}`,
         },
       });
 
+      // Update the user context with the fetched user data
       setUser(userResponse.data);
 
+      // Navigate the user to the "/posts" route
       navigate("/posts");
     } catch (err) {
       console.log(err);
-      alert(err.response.data.error);
+      alert(err.response.data.error); // Display an alert with the error message
     }
   };
 
+  // JSX rendering of the Login component
   return (
     <>
       <h1>Login</h1>
--- client/src/users/Register.jsx
+++ client/src/users/Register.jsx
@@ -12,34 +12,43 @@ let emptyForm = {
 function Register({ setUser }) {
   const navigate = useNavigate();
 
+  // State for storing the form data
   let [form, setForm] = useState(emptyForm);
 
+  // Function to update form state when input values change
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
 
+  // Function to handle form submission
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
+      // Send a POST request to the "/auth/register" endpoint with the form data
       const authResponse = await axios.post("/auth/register", form);
       const token = authResponse.data.token;
 
+      // If no token is returned, clear the form and exit the function
       if (!token) {
         setForm(emptyForm);
         return;
       }
 
+      // Store the token in the browser's local storage
       localStorage.setItem("token", token);
 
+      // Send a GET request to "/api/users" to fetch user data using the token for authentication
       const userResponse = await axios.get("/api/users", {
         headers: {
           Authorization: `Bearer ${localStorage.getItem("token")}`,
         },
       });
 
+      // Set the user state with the fetched user data
       setUser(userResponse.data);
 
+      // Navigate to the "/posts" route
       navigate("/posts");
     } catch (err) {
       console.log(err);
