f6f3260ff40383dc0e61e3da0895e3513abb9288
ROBHACKZ1257 <rob.promail47@gmail.com>
2023-08-25 13:18:12 -0600

    Updated the Models directory with a userfile

---
+++ backend/models/posts.js
@@ -0,0 +1,16 @@
+const posts = [
+    {
+        title: 'Shut up and give toilet paper DONKAY!',
+        message: 'Once upon a time.',
+    },
+    {
+        title: 'I lost to Robert in chess',
+        message: 'I suck at playing chess guys',
+    },
+    {
+        title: 'Tactical nuke incoming!!!',
+        message: '*NUCLEAR SIREN GOING OFF*',
+    }
+ ]
+ 
+ module.exports = posts
