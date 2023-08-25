d1ab2d3ac3db8ca40e842068772c773f314b53fb
lyalex011 <lytvolex@gmail.com>
2023-08-25 13:14:36 -0600

    some tailwind

--- client/src/pages/IndexPost.jsx
+++ client/src/pages/IndexPost.jsx
@@ -24,17 +24,17 @@ function IndexPost() {
     }, [])
 
     return (
-            <div>
-                <h1>Posts</h1>
-                <div id="posts">
+            <div className="my-32">
+                <h1 className="flex justify-center mb-8 text-large font-extrabold leading-none tracking-tight text-lime-900 md:text-5xl lg:text-6xl">Posts</h1>
+                <div  id="posts">
                     {posts.map((post,i)=>{
-                        <div>
-                            <h2>{post.title}</h2>
-                            <p>{post.message}</p>
+                        <div className="w-full p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 ">
+                            <h2 className="mb-2 text-3xl font-bold text-lime-500 ">{post.title}</h2>
+                            <p className="mb-5 text-base text-gray-500 sm:text-lg ">{post.message}</p>
                         </div>
                     })}
                 </div>
-                <button onClick={() => navigate('/new')}>NEW POST</button>
+                <button className="mt-6 bg-lime-600 inline-block w-full rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-black shadow-[0_4px_9px_-4px_#00FF00 ] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] " onClick={() => navigate('/new')}>NEW POST</button>
             </div>
     )
 }
