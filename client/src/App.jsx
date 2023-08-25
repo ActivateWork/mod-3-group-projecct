472851ed98fea66f5be7dcd24e9e15216436b4a0
Vivian Rice <weixu1220@gmail.com>
2023-08-25 12:40:22 -0600

    profile

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,21 @@ import IndexPost from './pages/IndexPost';
 import NewPost from './pages/NewPost';
 import Navbar from './components/NavBar';
 function App() {
-  
+  const[ user, setUser] = useState({})
+
+  async function getUser(){
+    try{
+      const response = await axios.get('api/users',{
+        headers: {
+       Authorization: `Bearer ${localStorage.getItem('token')}`
+      }
+    })
+    console.log(response)
+    setUser(response.data)
+    }catch(err){
+      console.log(err.message)
+    }
+  }
 
   useEffect(()=>{
     console.log('page loaded')
